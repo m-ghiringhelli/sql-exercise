86d04ce4364a5e6361581f0762a876c02e564f80
m-ghiringhelli <mghiring@gmail.com>
2022-06-15 15:18:37 -0700

    test 11 passes

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,11 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT title, COUNT(rental_id)
+FROM rental
+LEFT JOIN inventory ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film on inventory.film_id = film.film_id
+WHERE rental_id IS NOT NULL
+GROUP BY film.film_id
+ORDER BY count DESC
+LIMIT 10
